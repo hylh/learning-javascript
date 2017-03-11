69fd0b7076792fa2cf26331020bdc4abb49db542
Roberth Hansen <roberth.hansen@uit.no>
2017-03-11 17:43:34 +0100

    fixed local and remote db address

--- app.js
+++ app.js
@@ -48,55 +48,27 @@ app.use(function(err, req, res, next) {
 
 var pg = require('pg');
 
-// create a config to configure both pooling behavior
-// and client options
-// note: all config is optional and the environment variables
-// will be read if the config is not present
+pg.defaults.ssl = true;
 var config = {
   user: 'hylh', //env var: PGUSER
   database: 'hero', //env var: PGDATABASE
   //password: 'secret', //env var: PGPASSWORD
-  //host: 'localhost', // Server hosting the postgres database
-  //port: 5432, //env var: PGPORT
-  //max: 10, // max number of clients in the pool
-  //idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
+  host: 'localhost', // Server hosting the postgres database
+  port: 5432, //env var: PGPORT
+  max: 10, // max number of clients in the pool
+  idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
 };
+var connString = process.env.DATABASE_URL || config
 
+pg.connect(connString, function(err, client) {
+  if (err) throw err;
+  console.log('Connected to postgres! Getting schemas...');
 
-//this initializes a connection pool
-//it will keep idle connections open for a 30 seconds
-//and set a limit of maximum 10 idle clients
-var pool = new pg.Pool(config);
-
-// This is special for heroku
-//var pool = new pg.Pool(process.env.DATABASE_URL);
-
-// to run a query we can acquire a client from the pool,
-// run a query on the client, and then return the client to the pool
-pool.connect(function(err, client, done) {
-  if(err) {
-    return console.error('error fetching client from pool', err);
-  }
-  client.query('SELECT $1::text as name', ['brianc'], function(err, result) {
-    //call `done(err)` to release the client back to the pool (or destroy it if there is an error)
-    done(err);
-
-    if(err) {
-      return console.error('error running query', err);
-    }
-    console.log(result.rows[0]);
-    //output: 1
-  });
+  client
+    .query('SELECT table_schema,table_name FROM information_schema.tables;')
+    .on('row', function(row) {
+      console.log(JSON.stringify(row));
+    });
 });
-
-pool.on('error', function (err, client) {
-  // if an error is encountered by a client while it sits idle in the pool
-  // the pool itself will emit an error event with both the error and
-  // the client which emitted the original error
-  // this is a rare occurrence but can happen if there is a network partition
-  // between your application and the database, the database restarts, etc.
-  // and so you might want to handle it and at least log it out
-  console.error('idle client error', err.message, err.stack)
-})
 
 module.exports = app;
