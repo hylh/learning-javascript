061489cf85b982ce7b1a521ade097188d04bae2c
Roberth Hansen <roberth.hansen@uit.no>
2017-03-11 17:20:21 +0100

    chanigng db address

--- app.js
+++ app.js
@@ -56,7 +56,7 @@ var config = {
   user: 'hylh', //env var: PGUSER
   database: 'hero', //env var: PGDATABASE
   //password: 'secret', //env var: PGPASSWORD
-  host: 'localhost', // Server hosting the postgres database
+  //host: 'localhost', // Server hosting the postgres database
   port: 5432, //env var: PGPORT
   max: 10, // max number of clients in the pool
   idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
@@ -66,10 +66,10 @@ var config = {
 //this initializes a connection pool
 //it will keep idle connections open for a 30 seconds
 //and set a limit of maximum 10 idle clients
-//var pool = new pg.Pool(config);
+var pool = new pg.Pool(config);
 
 // This is special for heroku
-var pool = new pg.Pool(process.env.DATABASE_URL);
+//var pool = new pg.Pool(process.env.DATABASE_URL);
 
 // to run a query we can acquire a client from the pool,
 // run a query on the client, and then return the client to the pool
