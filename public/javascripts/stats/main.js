36933da01af088c0f2b490071d2dbcc03a4e6773
Roberth Hansen <roberth.hansen@uit.no>
2017-05-08 16:29:04 +0200

    added stats page

---
+++ public/javascripts/stats/main.js
@@ -0,0 +1,64 @@
+google.charts.load('current', {packages: ['table']});
+google.charts.setOnLoadCallback(initialDrawTable);
+
+const monthStringArray = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+var table;
+
+function initialDrawTable() {
+    // Define the chart to be drawn.
+    let data = new google.visualization.DataTable();
+    let feb = 2;
+    getMonth(feb).then(function(monthData){
+        data.addColumn('number', monthStringArray[feb-1]);
+        data.addColumn('number', 'Temperature');
+        data.addColumn('number', 'Humidity');
+        data.addColumn('number', 'Pressure');
+        let rows = createChartRows(monthData);
+        data.addRows(rows);
+        table = new google.visualization.Table(document.getElementById('table'));
+        table.draw(data, null);
+    });
+}
+
+$(document).ready(function(){
+    $('select').selectpicker({
+        style: 'btn-primary',
+    });
+    $('#primaryMonth').selectpicker({
+        title: 'Choose month'
+    });
+});
+
+$('#primaryMonth').on('change' , function(){
+    let newMonth = $("#primaryMonth option:selected").text();    
+    let month = monthStringArray.indexOf(newMonth);
+    updateChart(month);
+});
+
+function updateChart(month) {
+    let data = new google.visualization.DataTable();
+    getMonth(month + 1).then(function(monthData){
+       data.addColumn('number', monthStringArray[month]);
+        data.addColumn('number', 'Temperature');
+        data.addColumn('number', 'Humidity');
+        data.addColumn('number', 'Pressure');
+        let rows = createChartRows(monthData);
+        data.addRows(rows);
+        table.draw(data, null);
+    });
+}
+
+function createChartRows(month) {
+    let array = [];
+    for(i=0; i < month.length; i++){
+        array[i] = [month[i].day, month[i].temperature, month[i].humidity, month[i].pressure];
+    }
+    return array;
+}
+
+function getMonth(month){
+    const searchString = '/model/month/' + month;
+    return $.get(searchString).then(function(data){
+        return data;
+    });
+}
